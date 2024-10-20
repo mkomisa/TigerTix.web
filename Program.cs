4a0df5f573fe7d3bb1a505ccf1c802928c07cecb
mkomisa <124744292+mkomisa@users.noreply.github.com>
2024-10-20 10:09:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+// Serve static files from the wwwroot folder
+app.UseStaticFiles();
+
+// Map the root URL ("/") to serve the HomePage.html file
+app.MapGet("/", async context =>
+{
+    context.Response.Redirect("/HomePage.html");
+});
+
+app.Run();
