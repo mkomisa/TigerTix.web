1102dbc25b360de94f0e17a5acc72194b0b12146
FJF326 <ffiumef@clemson.edu>
2024-10-30 15:11:49 -0400

    working signin and signup

--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -2,6 +2,7 @@
 {
     public class User
     {
+        public int UserId {get; set;}
         public string Username { get; set; }
         public string Password {get; set; }
         public string FirstName { get; set; }
