8caf20b9debed2cb0dd603748179851d1f6a9d8c
FJF326 <ffiumef@clemson.edu>
2024-10-31 11:41:32 -0400

    starting to setup Event Manager

---
+++ Data/Entities/EventManagers.cs
@@ -0,0 +1,11 @@
+namespace TigerTix.web.Data.Entities
+{
+    public class EventManager
+    {
+        public int UserId {get; set;}
+        public string Username { get; set; }
+        public string Password {get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
