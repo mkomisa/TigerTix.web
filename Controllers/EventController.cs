3fb64846037dd613396b6c7c513c56235b1b0321
FJF326 <ffiumef@clemson.edu>
2024-10-31 16:50:17 -0400

    get function working for event api

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -5,8 +5,40 @@ using TigerTix.web.Data.Entities;
 
 namespace TigerTix.web.Controllers
 {
-public class EventController : BaseController{
+[Route("/api/events")]
+public class EventController : ControllerBase{
+    private string _connectionString;
+    public EventController(IConfiguration configuration)
+    {
+        //get connection string from appsettings.json            
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    [HttpGet]
+    public IActionResult Get(){
+        var events= new List<Event>();
+
+        using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Events", connection);
+                var reader = command.ExecuteReader();
+                Event myevent =new Event();
+
+                while(reader.Read()){
+                    myevent.EventId = (int)reader["EventId"];
+                    myevent.Title = reader["Title"].ToString();
+                    myevent.Date = reader["Date"].ToString();
+                    myevent.Time = reader["Time"].ToString();
+                    myevent.Location = reader["Location"].ToString();
+                    events.Add(myevent);
+                }
+
+    }
+    return Ok(events);
 
 }
 }
+}
+
 
