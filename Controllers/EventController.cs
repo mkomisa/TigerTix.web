51a763e58c0e1c075c738cad80c1e0dcaddc91aa
FJF326 <ffiumef@clemson.edu>
2024-10-31 22:50:33 -0400

    working post event

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using TigerTix.web.Data.Entities;
 
 namespace TigerTix.web.Controllers
 {
 [Route("/api/events")]
-public class EventController : ControllerBase{
+public class EventController : Controller{
     private string _connectionString;
     public EventController(IConfiguration configuration)
     {
@@ -23,21 +26,47 @@ public class EventController : ControllerBase{
                 connection.Open();
                 var command = new SqlCommand("SELECT * FROM Events", connection);
                 var reader = command.ExecuteReader();
-                Event myevent =new Event();
-
+               
+                
                 while(reader.Read()){
-                    myevent.EventId = (int)reader["EventId"];
-                    myevent.Title = reader["Title"].ToString();
-                    myevent.Date = reader["Date"].ToString();
-                    myevent.Time = reader["Time"].ToString();
-                    myevent.Location = reader["Location"].ToString();
-                    events.Add(myevent);
+                     Event myevent = new Event();
+                        myevent.EventId = (int)reader["EventId"];
+                        myevent.Title = reader["Title"].ToString();
+                        myevent.Date = reader["Date"].ToString();
+                        myevent.Time = reader["Time"].ToString();
+                        myevent.Location = reader["Location"].ToString();   
+                        events.Add(myevent);
                 }
 
     }
-    return Ok(events);
+        return Ok(events);
 
 }
+    [HttpPost]
+    public IActionResult Post([FromBody]Event newEvent )
+    {
+        using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                 var command = new SqlCommand("INSERT INTO events (Title, Date, Time, Location) VALUES (@Title, @Date, @Time, @Location)", connection);
+                command.Parameters.AddWithValue("@Title", newEvent.Title);
+                command.Parameters.AddWithValue("@Date", newEvent.Date);
+                command.Parameters.AddWithValue("@Time", newEvent.Time);
+                command.Parameters.AddWithValue("@Location", newEvent.Location);
+
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    return Ok("Event added successfully");
+                }
+                else
+                {
+                    return StatusCode(500, "An error occurred while adding the event");
+                }
+            }
+        }
+
+                
 }
 }
 
