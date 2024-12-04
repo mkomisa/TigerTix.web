19f7f33f550090d6020e331789faa3a4c2dea03d
FJF326 <ffiumef@clemson.edu>
2024-12-03 20:38:11 -0500

    added get ticket api

--- Controllers/TicketController.cs
+++ Controllers/TicketController.cs
@@ -1,3 +1,4 @@
+using Data.Entities.Ticket.cs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
@@ -16,9 +17,26 @@ namespace TigerTix.web.Controllers
     [Route("api/ticket")]
     [HttpGet]
     public IActionResult getTicketsByEventID(string eventID){
-        //implement this
-        return Ok();
-    } 
+        List<Ticket> tickets= new List<Ticket>();
+         using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("SELECT * FROM Tickets WHERE EventID=@eventID", connection);
+            command.Parameters.AddWithValue("@EventID", "%" + eventID + "%");
+            var reader = command.ExecuteReader();
 
+             while (reader.Read())
+                {
+                    Ticket myTicket = new Ticket
+                    {
+                        EventId = (int)reader["EventId"],
+                        ticketName = reader["TicketName"].ToString(),
+                        price = (double)reader["Price"]
+                    };
+                    tickets.Add(myTicket);
+                }
+        }
+        return Ok(tickets);
+    } 
     }
 }
